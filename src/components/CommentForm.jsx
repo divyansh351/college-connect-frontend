67063edbbe0313664d86a6e3ca79e900d3f94f77
Gaurav Prateek <131291701+Tennyson100@users.noreply.github.com>
2023-12-22 11:09:29 +0530

    CommentForm

---
+++ src/components/CommentForm.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+
+const PostForm = ({ post_id }) => {
+    const [formData, setformData] = useState({
+        content: '',
+        post_id: post_id
+    })
+    const [success, setSuccess] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
+    const handleChange = (evt) => {
+        const fieldName = evt.target.name;
+        const value = evt.target.value;
+        setformData((currdata) => ({
+            ...currdata,
+            [fieldName]: value,
+        }));
+    };
+
+    const handleSubmit = async (evt) => {
+        evt.preventDefault();
+
+        setLoading(true);
+        setSuccess(false);
+        setError(false);
+        const token = localStorage.getItem('token');
+
+
+        try {
+            const response = await axios.post(
+                `https://college-connect-backend-0x0i.onrender.com/comment/new`,
+                formData,
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                }
+            )
+            setLoading(false);
+            if (response.data.message == 'successfully posted') {
+                setSuccess(true)
+                window.location.reload();
+            }
+            else {
+                setError(true);
+                setErrorMessage(response.data.message)
+            }
+        } catch (err) {
+            setLoading(false);
+            setError(true);
+            setErrorMessage(err.response.data.message)
+        }
+    };
+    return (
+        <div>
+            <form>
+                <div>
+                    <label htmlFor="content">Content:</label>
+                    <textarea
+                        id="content"
+                        value={formData.content}
+                        name="content"
+                        onChange={handleChange}
+                    />
+                </div>
+                <button type="button" onClick={handleSubmit}>
+                    Comment
+                </button>
+            </form>
+            {success ? <div>Comment Posted</div> : <></>}
+            {error ? <div>{errorMessage}</div> : <></>}
+            {loading ? <div>Registering</div> : <></>}
+        </div>
+    );
+};
+
+export default PostForm;
