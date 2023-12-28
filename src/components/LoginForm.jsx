82f635d09101dfd6e689f43c1a41e4958db42f1f
Gaurav Prateek <131291701+Tennyson100@users.noreply.github.com>
2023-12-28 19:12:27 +0530

    loginFormStyles

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,5 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
@@ -13,19 +12,18 @@ import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import axios from 'axios';
 import AuthVerify from '../helper/JWTVerify';
-
+import './LoginForm.css'; // Import the stylesheet
 export default function LoginForm() {
     const [showPassword, setShowPassword] = useState(false);
     const [formData, setformData] = useState({
-        username: "",
-        password: ""
-    })
+        username: '',
+        password: '',
+    });
     const [success, setSuccess] = useState(false);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
     const navigate = useNavigate();
-
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
     const handleChange = (evt) => {
@@ -36,6 +34,7 @@ export default function LoginForm() {
             [fieldName]: value,
         }));
     };
+
     const handleSubmit = async (evt) => {
         evt.preventDefault();
 
@@ -49,49 +48,48 @@ export default function LoginForm() {
                 formData,
                 {
                     headers: {
-                        "Content-type": "application/json; charset=UTF-8"
+                        'Content-type': 'application/json; charset=UTF-8',
                     },
                 }
-            )
+            );
             setLoading(false);
-            if (response.data.message == 'Login Successful') {
-                localStorage.setItem("token", response.data.token)
-                localStorage.setItem("role", response.data.role)
-                setSuccess(true)
-                navigate("/profile");
-            }
-            else {
+            if (response.data.message === 'Login Successful') {
+                localStorage.setItem('token', response.data.token);
+                localStorage.setItem('role', response.data.role);
+                setSuccess(true);
+                navigate('/profile');
+            } else {
                 setError(true);
-                setErrorMessage(response.data.message)
+                setErrorMessage(response.data.message);
             }
         } catch (err) {
             setLoading(false);
             setError(true);
-            setErrorMessage(err.response.data.message)
+            setErrorMessage(err.response.data.message);
         }
     };
 
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem('token');
     useEffect(() => {
         setLoading(true);
         if (AuthVerify(token)) navigate('/profile');
         setLoading(false);
-    }, [])
+    }, []);
+
     return (
         <>
-            <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
-
-                <div>
-                    <h1>Login-Form</h1>
+            <Box className="login-container">
+                <div className="form-container">
+                    <h1>College-Connect</h1>
                     <TextField
                         label="Username"
                         id="outlined-start-adornment"
-                        sx={{ m: 1, width: '25ch' }}
-                        value={formData.Username}
-                        name='username'
+                        sx={{ m: 1, width: '100%' }}
+                        value={formData.username}
+                        name="username"
                         onChange={handleChange}
                     />
-                    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                    <FormControl sx={{ m: 1, width: '100%' }} variant="outlined">
                         <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
                         <OutlinedInput
                             id="outlined-adornment-password"
@@ -108,17 +106,23 @@ export default function LoginForm() {
                                 </InputAdornment>
                             }
                             label="Password"
-                            value={formData.Password}
-                            name='password'
+                            value={formData.password}
+                            name="password"
                             onChange={handleChange}
                         />
                     </FormControl>
+                    <Button
+                        onClick={handleSubmit}
+                        variant="contained"
+                        sx={{ marginTop: '20px', backgroundColor: '#3498db', color: '#fff' }}
+                    >
+                        Login
+                    </Button>
                 </div>
             </Box>
-            <Button onClick={handleSubmit} variant="contained">Submit</Button>
-            {success ? <div>Login Succesful</div> : <></>}
-            {error ? <div>{errorMessage}</div> : <></>}
-            {loading ? <div>Please Wait...</div> : <></>}
+            {success ? <div sx={{ color: '#4CAF50', marginTop: '20px' }}>Login Successful</div> : <></>}
+            {error ? <div sx={{ color: '#ff0000', marginTop: '20px' }}>{errorMessage}</div> : <></>}
+            {loading ? <div sx={{ marginTop: '20px' }}>Please Wait...</div> : <></>}
         </>
     );
 }
