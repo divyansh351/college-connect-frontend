2199d9c271698065ad90d5d91168f2f28762ce24
Gaurav Prateek <131291701+Tennyson100@users.noreply.github.com>
2023-12-29 23:14:21 +0530

    CourseList

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import CourseCard from "./CourseCard";
+import backgroundImage from "../assets/bgimg.svg"; // Adjust the path accordingly
 
 export default function CourseList() {
     const [courses, setcourses] = useState([]);
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -16,19 +18,37 @@ export default function CourseList() {
 
         fetchData();
     }, []);
+
     return (
         <div className="course-list-container">
-            {courses.map((course, index) => (
-                <CourseCard key={index} id={course._id} linkDisable={false} />
-            ))}
+            <div className="background-container" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', minHeight: '100vh', opacity: 0.5 }} />
+            <div className="content-container">
+                {courses.map((course, index) => (
+                    <CourseCard key={index} id={course._id} linkDisable={false} />
+                ))}
+            </div>
             <style jsx="true">{`
-            .course-list-container {
-                display: flex;
-                flex-wrap: wrap;
-                justify-content: space-around;
-                margin: 20px; /* Adjust margin as needed */
-            }
-        `}</style>
+                .course-list-container {
+                    position: relative;
+                }
+
+                .background-container {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                    z-index: -1;
+                }
+
+                .content-container {
+                    display: flex;
+                    flex-wrap: wrap;
+                    justify-content: space-around;
+                    margin: 20px; /* Adjust margin as needed */
+                    z-index: 1; /* Ensure content is above the background */
+                }
+            `}</style>
         </div>
     );
 }
