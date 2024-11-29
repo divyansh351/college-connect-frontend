fd0333fb3501ba5dc3df8f11a0f35ea290f9e7ea
Divyansh Gupta <94603079+divyansh351@users.noreply.github.com>
2024-11-29 13:19:52 +0530

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -25,7 +25,7 @@ const LandingPage = () => {
                         </>
                     }
                 </div>
-                <h1 className='land-heading'>College Connect</h1>
+                <h1 className='land-heading'>Campus Connect</h1>
                 <p>Welcome to College Connect, dedicated to the students of IIT (ISM) Dhanbad. Our platform is designed
                     to help you discover and understand the wealth of courses offered at your esteemed institution.</p>
                 <a href="/course" className="cta-button">View Courses</a>
